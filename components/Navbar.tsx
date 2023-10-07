524d335383c96a8ff75b0d9f6e20b8c4aa5d0fd0
blind675 <2725079+blind675@users.noreply.github.com>
2023-10-07 16:42:21 +0300

    First commit

---
+++ components/Navbar.tsx
@@ -0,0 +1,37 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+function Navbar() {
+  const navIcons = [
+    { src: "/assets/icons/user.svg", alt: "user", href: "/profile" },
+  ];
+
+  return (
+    <header className="w-full bg-white">
+      <nav className="p-4 flex justify-between">
+        <Link href="/" className="flex items-center gap-1">
+          <Image src="/assets/logo.svg" width={36} height={36} alt="logo" />
+
+          <p className="ml-2 text-2xl font-bold">
+            Open <span className="text-primary">360</span>
+          </p>
+        </Link>
+
+        <div className="flex gap-4 items-center">
+          {navIcons.map((icon, index) => (
+            <Link
+              key={index}
+              href={icon.href}
+              className="hover:border-b-2 hover:border-primary"
+            >
+              <Image src={icon.src} width={28} height={28} alt={icon.alt} />
+            </Link>
+          ))}
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+export default Navbar;
---
+++ components/Searchbox.tsx
@@ -0,0 +1,94 @@
+"use client";
+
+import React from "react";
+
+function Searchbox() {
+  const [isDropdownOpened, toggleDropdown] = React.useState(false);
+
+  return (
+    <form className="flex justify-center">
+      <div className="flex w-[1000px]">
+        <div className=" flex-col">
+          <button
+            className="h-10 w-44 flex-shrink-0 z-10 inline-flex items-center py-2.5 px-4 text-sm font-medium text-center text-gray-900 bg-gray-100 border border-gray-300 rounded-l-lg hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700 dark:text-white dark:border-gray-600"
+            type="button"
+            onClick={() => toggleDropdown(!isDropdownOpened)}
+          >
+            All categories{" "}
+            <svg
+              className="w-2.5 h-2.5 ml-2.5"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 10 6"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="m1 1 4 4 4-4"
+              />
+            </svg>
+          </button>
+          <div
+            className={`${
+              isDropdownOpened ? "" : "hidden"
+            } z-10 bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 `}
+          >
+            <ul className="py-2 text-sm text-gray-700 dark:text-gray-200">
+              <li>
+                <button
+                  type="button"
+                  className="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                >
+                  Logos
+                </button>
+              </li>
+              <li>
+                <button
+                  type="button"
+                  className="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                >
+                  Mockups
+                </button>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="relative w-full">
+          <input
+            type="search"
+            id="search-dropdown"
+            className="block p-2.5 w-full h-10 z-20 text-sm text-gray-900 bg-gray-50 rounded-r-lg border-l-gray-50 border-l-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-l-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500"
+            placeholder="Search for an Open Science Project"
+            required
+          />
+          <button
+            type="submit"
+            className="absolute top-[1px] right-0 p-2.5 text-sm font-medium  text-white bg-primary rounded-r-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            <svg
+              className="w-4 h-4"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 20 20"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+              />
+            </svg>
+            <span className="sr-only">Search</span>
+          </button>
+        </div>
+      </div>
+    </form>
+  );
+}
+
+export default Searchbox;
