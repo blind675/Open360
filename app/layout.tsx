ce9df8a4bad2a358fc3b03fbea38ed41097866c1
blind675 <2725079+blind675@users.noreply.github.com>
2023-10-08 16:25:07 +0300

    Add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import Navbar from "@/components/Navbar";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,6 +23,7 @@ export default function RootLayout({
         <main className="max-w-10xl mx-auto bg-slate-200">
           <Navbar />
           {children}
+          <Analytics />
         </main>
       </body>
     </html>
